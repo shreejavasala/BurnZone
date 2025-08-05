dc0a6a2371f34ac27fc92e6809a6654aecd967b7
shreejavasala <shreevasala29@gmail.com>
2025-08-05 21:29:40 +0530

    Create ExerciseCard.js

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -4,8 +4,19 @@ import { Button, Stack, Typography } from '@mui/material';
 
 const ExerciseCard = ({ exercise }) => {
   return (
-    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+    <Link className="exercise-card" to={`/exercise/${exercise.exerciseId}`}>
       <img src={exercise.gifUrl} alt={exercise.name} Loading="lazy"></img>
+      <Stack direction="row">
+        <Button sx={{ml: '21px', color: '#FFF', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
+          {exercise.bodyParts[0]}
+        </Button>
+        <Button sx={{ml: '21px', color: '#FFF', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}>
+          {exercise.targetMuscles[0]}
+        </Button>
+      </Stack>
+      <Typography ml="21px" color="#000" fontWeight="bold" mt="11px" pb="10px" textTransform="capitalize" fontSize="22px">
+        {exercise.name}
+      </Typography>
     </Link>
   )
 }
